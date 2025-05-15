e3c62e9a163324a1d33a9864d12b095d7256a0ce
Carolina Poluboiarinov <153758435+carolpoluba@users.noreply.github.com>
2025-05-15 15:53:38 -0300

    1st commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade é igual a ${vel} Km/h.`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida e foi multado!')
+}
+console.log('Dirija sempre utilizando cinto de segurança!')
