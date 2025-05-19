15d67fe06c333f40a7c8791dff60fe9f62292b00
Carolina Poluboiarinov <153758435+carolpoluba@users.noreply.github.com>
2025-05-19 18:46:46 -0300

    ex015 resolvido

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -8,5 +8,42 @@ function verificar() {
     } else {
         var formssex = window.document.getElementsByName('radsex')
         var idade = ano - Number(formsano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (formssex[0].checked) {
+            genero = 'Masculino'
+            if (idade >=0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'babyboy.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'youngman.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'adultman.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'oldman.png')
+            }
+        } else if (formssex[1].checked) {
+            genero = 'Feminino'
+            if (idade >=0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'babygirl.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'youngwoman.png')
+            } else if (idade < 50) {
+                // adulta
+                img.setAttribute('src', 'adultwoman.png')
+            } else {
+                // idosa
+                img.setAttribute('src', 'oldwoman.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Foi detectado o gênero ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
