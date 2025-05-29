230194da72b1429c4a84801b21d972b924d40051
Carolina Poluboiarinov <153758435+carolpoluba@users.noreply.github.com>
2025-05-29 17:27:37 -0300

    ex016 resolvido

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,33 @@
-alert('Olá!')
+function contar() {
+    var inicio = window.document.getElementById('txtini')
+    var fim = window.document.getElementById('txtfim')
+    var passo = window.document.getElementById('txtpas')
+    var res = window.document.getElementById ('res') 
+    
+    if (txtini.value.length == 0 || txtfim.value.length == 0 || txtpas.value.length ==0) {
+        window.alert('[ERRO] Há dados faltando!')
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando o valor do passo como 1.')
+            p = 1
+        }
+
+        //contagem crescente
+        if(i < f) {
+            for(var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // contagem regressiva
+            for(var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
