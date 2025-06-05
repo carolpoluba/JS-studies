2a69e52785a6dabae854eadfa87b633860f51740
Carolina Poluboiarinov <153758435+carolpoluba@users.noreply.github.com>
2025-06-04 23:00:44 -0300

    exercício 17 resolvido

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function calcular() {
+    let num = window.document.getElementById('txtn')
+    let tab = window.document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1; // var iniciando em 1
+        tab.innerHTML = ' ' // limpa a tabuada antes de outro result
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+
+}
